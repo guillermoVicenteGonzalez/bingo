1142651d0ba40196b9aa43654226536dda3f7355
guillermoVicenteGonzalez <guillermovg@usal.es>
2022-12-12 13:42:42 +0100

    created settings

--- front/bingo-app/src/components/personalCard.vue
+++ front/bingo-app/src/components/personalCard.vue
@@ -1,11 +1,22 @@
 <template>
-    <v-container grid-list-xs>
+    <v-container>
         <v-text-field></v-text-field>
     </v-container>
 </template>
 
 <script setup>
     import axios from "axios";
+    import {ref} from "vue";
 
+    const props =  defineProps({
+        name:String,
+        id:Number
+    });
+
+    var card = ref();
+
+    async function getPersonalCard(){
+
+    }
 
 </script>
