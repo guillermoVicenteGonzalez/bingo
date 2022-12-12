a75f5446e052d3141e9be4a20fe067be41d985ce
guillermoVicenteGonzalez <guillermovg@usal.es>
2022-12-12 12:52:40 +0100

    cambios

---
+++ front/bingo-app/src/components/personalCard.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-container grid-list-xs>
+        <v-text-field></v-text-field>
+    </v-container>
+</template>
+
+<script setup>
+    import axios from "axios";
+
+
+</script>
--- front/bingo-app/src/router/index.js
+++ front/bingo-app/src/router/index.js
@@ -1,5 +1,6 @@
 import CardCollectionVue from '@/components/CardCollection.vue'
 import Landing from '@/components/Landing.vue'
+import personalCardVue from '@/components/personalCard.vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 
@@ -9,7 +10,8 @@ const routes = [
     name: 'home',
     component:Landing
   },
-  {path:"/collection",name:"collection", component:CardCollectionVue}
+  {path:"/collection",name:"collection", component:CardCollectionVue},
+  {path:"/card",name:"personalCard", component:personalCardVue}
 ]
 
 const router = createRouter({
